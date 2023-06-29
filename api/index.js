27a71760e616416cf2c0149383257342ce6f28aa
Kieren Enright <kierenenright27@gmail.com>
2023-06-28 21:12:09 -0700

    install and add JWT auth, create file (users.js) for usersRouter

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 const apiRouter = require('express').Router();
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
 
 apiRouter.get('/', (req, res, next) => {
   res.send({
@@ -12,6 +14,36 @@ apiRouter.get('/health', (req, res, next) => {
   });
 });
 
+apiRouter.use(async (req, _res, next) => {
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  } else {
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${prefix}`
+    });
+  }
+});
+
 // place your routers here
+const usersRouter = require('./users');
+apiRouter.use('/users', usersRouter);
 
 module.exports = apiRouter;
---
+++ api/users.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const usersRouter = express.Router();
+const jwt = require('jsonwebtoken');
+
+module.exports = usersRouter;
