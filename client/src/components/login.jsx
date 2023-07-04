d0d835f4e6e0257d73936ef9f0afbcfb05f83e72
Shruthi Tuplur <shruthituplur@gmail.com>
2023-07-04 09:31:44 -0700

    added structuring to sign in page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -5,14 +5,18 @@ const Login = () => {
     return(
         <div id='login-main-div'>
             <h2>Sign into your guitarget account</h2>
-            <form>
-                <label htmlFor="email-or-username" >Email or username</label>
-                <input required type='text' name='email-or-username'></input>
-                <label htmlFor="password">Password</label>
-                <input required type='password' name='password'></input>
-                <button type='submit'>Sign in</button>
+            <form id='login-form'>
+                <div id='login-username-div'>
+                    <label htmlFor="email-or-username" >Email or username</label>
+                    <input required type='text' name='email-or-username' id='login-username-input'></input>
+                </div>
+                <div id='login-password-div'>
+                    <label htmlFor="password">Password</label>
+                    <input required type='password' name='password' id='login-password-input'></input>
+                </div>
+                <button type='submit' id='sign-in-button'>Sign in</button>
             </form>
-
+        <button id='login-create-acc-button'> Create your guitarget account</button>
         </div>
     )
 }
