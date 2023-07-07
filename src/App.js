3e9c05ce6a2975f95fe291850acd811b74608838
Raymond Nhan <nhanraymond@gmail.com>
2023-07-06 20:23:56 -0700

    products path

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-import React, {useState, useEffect} from 'react';
-import { Route } from 'react-router-dom'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
 import {
   Header,
   DefaultHomepage,
   AllGuitars,
   Login,
-  Register
-} from './components'
+  Register,
+  Products,
+} from "./components";
 
 function App() {
-  const [username, setUsername] = useState('')
-  const [token, setToken] = useState(null)
-  const [user, setUser] = useState([])
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = React.useState(0);
+  const [username, setUsername] = useState("");
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState([]);
 
   return (
-    
     <div className="App">
-       <Header token={token} setToken={setToken} setUser={setUser}/>
-      <Route path='/users/login'>
-        <Login username={username} setUsername={setUsername} setToken={setToken} setUser={setUser}/>
+      <Header token={token} setToken={setToken} setUser={setUser} />
+      <Route path="/users/login">
+        <Login
+          username={username}
+          setUsername={setUsername}
+          setToken={setToken}
+          setUser={setUser}
+        />
       </Route>
-      <Route path = '/users/register'>
-        <Register username = {username} setUsername={setUsername} setToken={setToken} setUser={setUser} />
+      <Route path="/users/register">
+        <Register
+          username={username}
+          setUsername={setUsername}
+          setToken={setToken}
+          setUser={setUser}
+        />
       </Route>
-       <Route path='/guitars'>
-          <AllGuitars />
-       </Route>
-       <Route exact path='/'>
-         <DefaultHomepage />
-       </Route>
-       
-      
+      <Route path="/guitars">
+        <AllGuitars />
+      </Route>
+      <Route>
+        <Products
+          path="/products"
+          products={products}
+          setProducts={setProducts}
+          count={count}
+          setCount={setCount}
+        />
+      </Route>
+      <Route exact path="/">
+        <DefaultHomepage />
+      </Route>
     </div>
-    
-  )
+  );
 }
 
 export default App;
