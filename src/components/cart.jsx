b2c7c166a5e67fd174f414e712ddfe61ce580862
Kieren Enright <kierenenright27@gmail.com>
2023-07-12 23:23:41 -0700

    Checkout button only appears in cart when there is a product in it now

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -71,9 +71,11 @@ function Cart({ user, cartData, setCartData }) {
           justifyContent: "flex-end",
         }}
       >
+        { cartData.length >= 1 && 
         <Button size="large" color="primary" onClick={handleCheckout}>
           Checkout
         </Button>
+        }
       </div>
     </>
   );
