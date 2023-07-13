bb829d590d928ac4d720ff2077f92e3d1f063000
Kieren Enright <kierenenright27@gmail.com>
2023-07-13 13:37:52 -0700

    WIP routers should be working as intended, still testing

--- api/history.js
+++ api/history.js
@@ -11,18 +11,16 @@ historyRouter.get('/:username', async (req, res, next) => {
     const { username } = req.params;
     try {
         const orderHistory = await getOrderHistory(username);
-        console.log("orderHistory: ", orderHistory);
         const orderIds = orderHistory.map(order => order.orderId);
-        console.log("orderIds: ", orderIds);
 
-        orderIds.forEach(async (id) => {
-            const orders = await getOrderById(id);
-            console.log("orders: ", orders);
-        });
+       const orderArr = await Promise.all(orderIds.map(async (id) => {
+        const order = await getOrderById(id);
+        return order;
+       }));
 
-        // orderHistory.forEach(order => async);
-        res.send(orderHistory);
-        
+        console.log("orderArr: ", orderArr);
+
+        res.send(orderArr);
     } catch (error) {
         next(error);
     }
@@ -33,17 +31,19 @@ historyRouter.post('/:username', async (req, res, next) => {
     try {
         const usersOrders = await getOrderByUser({username});
 
-        console.log("usersOrders: ", usersOrders);
-        
         const ids = usersOrders.map(order => order.id);
         const customersName = usersOrders.map(customer => customer.customerName);
         console.log("ids: ", ids);
         console.log("customersName: ", customersName);
 
-        ids.forEach(async (id) => {
+        const addToHistory = await Promise.all(ids.map(async (id) => {
             const addToHistory = await createOrderHistory(id, customersName[0]);
-            console.log("addToHistory: ", addToHistory);
-        });
+            return addToHistory;
+          }));
+
+
+          console.log("addToHistory: ", addToHistory);
+
         res.send(addToHistory);
     } catch (error) {
         next(error);
